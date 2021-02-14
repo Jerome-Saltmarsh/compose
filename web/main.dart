6ca06d0258183e12abc85c10360ec2d9e0f76790
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-02-15 00:50:44 +0100

    stable

--- web/main.dart
+++ web/main.dart
@@ -16,21 +16,34 @@ void main() {
   var app = querySelector('#app');
   app.text = 'Your Dart app is running. again again';
   app.style.backgroundColor = 'green';
-  app.style.minHeight = '500px';
+  app.minHeight = 500;
 
-  app.children.add(horizontal(items));
-  app.children.add(vertical(items, MainAlign.End));
-  app.children.add(horizontal(items));
+  // add(app, horizontal(items));
+  // add(app, horizontal(items, MainAlign.End));
+  // add(app, horizontal(items));
+
+  add(app, menu);
+}
+
+Element get menu {
+  return vertical([div('item 1'), div('item 2'), div('item 3')]);
+}
+
+extension ElementExtensions on Element {
+  set minHeight(int value) {
+    style.minHeight = '${value}px';
+  }
 }
 
 Element horizontal(List<Element> children,
-    [MainAlign mainAlign = MainAlign.Even, CrossAlign crossAlign = CrossAlign.Center]) {
+    [MainAlign mainAlign = MainAlign.Even,
+    CrossAlign crossAlign = CrossAlign.Center]) {
   return container(children, Direction.Horizontal, mainAlign, crossAlign);
 }
 
 Element vertical(List<Element> children,
-    [MainAlign MainAlign, CrossAlign CrossAlign = CrossAlign.Center]) {
-  return container(children, Direction.Vertical, MainAlign, CrossAlign);
+    [MainAlign mainAlign = MainAlign.Even, CrossAlign crossAlign = CrossAlign.Center]) {
+  return container(children, Direction.Vertical, mainAlign, crossAlign);
 }
 
 Element box(Element element) {
@@ -91,14 +104,6 @@ String convertCrossAlignToAlignItem(CrossAlign value) {
   throw Exception("could not parse item: $value");
 }
 
-List<Element> get items {
-  return [
-    Element.div()..text = 'child 1',
-    Element.div()..text = 'child 2',
-    Element.div()..text = 'child 3',
-    Element.div()..text = 'child 4'
-  ];
-}
 
 Element text(String value) {
   var d = div();
@@ -106,13 +111,40 @@ Element text(String value) {
   return d;
 }
 
-Element div([Element element1, List<Element> children]) {
+Element div([String text, Element element1, List<Element> children]) {
+
   var d = Element.div();
+
+  if (text != null) {
+    d.text = text;
+  }
 
   if (element1 != null) {
     d.children.add(d);
   }
 
-  d.children.addAll(children);
+  if (children != null) {
+    d.children.addAll(children);
+  }
+
   return d;
+}
+
+void addAll(Element parent, List<Element> children) {
+  parent.children.addAll(children);
+}
+
+void add(Element parent, Element child) {
+  parent.children.add(child);
+}
+
+////////////////
+
+List<Element> get items {
+  return [
+    Element.div()..text = 'child 1',
+    Element.div()..text = 'child 2',
+    Element.div()..text = 'child 3',
+    Element.div()..text = 'child 4'
+  ];
 }
